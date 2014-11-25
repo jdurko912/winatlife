b6fdb962b9ec163369ff1dc4cdea68f6fecc7b62
Joey <jnsilentbob912@gmail.com>
2014-11-25 18:20:52 -0500

    Ran Yeoman to create scaffolding.

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name winatlifeApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the winatlifeApp
+ */
+angular.module('winatlifeApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
